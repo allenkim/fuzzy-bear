333366b9a1d0163c3af4d5856cec4caeaabe6907
Allen Kim <akim003@citymail.cuny.edu>
2015-07-07 21:58:55 -0600

    Added teddy bears and explosions

--- js/load.js
+++ js/load.js
@@ -12,6 +12,7 @@ var loadState = {
         game.load.spritesheet('skull','assets/skull.png');
         game.load.spritesheet('explosion','assets/explosion.png',128,128);
         game.load.image('tileset','assets/tileset.png');
+        game.load.image('teddy','assets/teddybearprojectile.png');
         game.load.tilemap('map','assets/map.json',null,Phaser.Tilemap.TILED_JSON);
         },
     
--- js/play.js
+++ js/play.js
@@ -10,27 +10,54 @@ var playState = {
             explosionAnimation.anchor.setTo(0.5,0.8);
             explosionAnimation.animations.add('explosion');
         }
+
+        this.teddies = game.add.group();
+        for (var i = 0; i < 10; i++){
+            var teddy = this.teddies.create(0,0,'teddy',0,false);
+            teddy.anchor.setTo(0.5,0.5);
+            game.physics.arcade.enable(teddy);
+        }
+        this.teddies.setAll('checkWorldBounds',true);
+        this.teddies.setAll('outOfBoundsKill',true);
         
         this.skull = game.add.sprite(game.world.centerX,game.world.centerY,'skull');
         this.skull.anchor.setTo(0.5,0.5);
         this.skull.scale.setTo(0.1,0.1);
         
         game.physics.arcade.enable(this.skull);
-
-        this.skullSpeed = 500;
         
         this.cursor = game.input.keyboard.createCursorKeys();
         
         game.input.keyboard.addKeyCapture([Phaser.Keyboard.UP,
           Phaser.Keyboard.DOWN, Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT]);
  
-        game.input.keyboard.addKey(Phaser.Keyboard.E).onDown.add(this.explode,this);
-	},
+        game.input.keyboard.addKey(Phaser.Keyboard.E).onDown.add(this.skullExplode,this);
+    
+        this.skullSpeed = 300;
+        this.teddySpeed = 200;
+        this.cooldownTime = 1000;
+
+        this.nextTeddy = 0;
+    },
 
 	update: function() {
         this.skullUpdate();
         game.physics.arcade.collide(this.skull,this.layer3);
+        game.physics.arcade.overlap(this.skull,this.teddies,this.skullExplode,null,this);
+
+        if (this.nextTeddy < game.time.now){
+            this.createRandomTeddy();
+            this.nextTeddy = game.time.now + this.cooldownTime;
+        }
 	},
+
+    createRandomTeddy: function(){
+        var teddy = this.teddies.getFirstExists(false);
+        var x = (game.rnd.frac()<0.5)? 0 : game.world.width;
+        var y = (game.rnd.frac()<0.5)? 0 : game.world.height;
+        teddy.reset(x,y);
+        game.physics.arcade.moveToXY(teddy,this.skull.x,this.skull.y,this.teddySpeed);
+    },
     
     createWorld: function() {
         this.map = game.add.tilemap('map');
@@ -78,10 +105,17 @@ var playState = {
         }
     },
     
-    explode: function(){
+    skullExplode: function(skull,skullKiller){
         var explosionAnimation = this.explosions.getFirstExists(false);
         explosionAnimation.reset(this.skull.x,this.skull.y);
         explosionAnimation.play('explosion',30,false,true);
-        this.skull.kill();
+        skull.kill();
+        skullKiller.kill();
+
+        game.time.events.add(2000, this.quitGame, this);
+    },
+
+    quitGame: function(){
+        game.state.start('menu');
     }
 };
